64717270398cb6a108e24b0a0890795fd52dc8b2
Mohammed Ahmed <mfeti2175@gmail.com>
2024-01-24 17:19:38 +0300

    shipping static data

--- src/Hooks/useGenre.ts
+++ src/Hooks/useGenre.ts
@@ -1,10 +1,11 @@
-import useData from "./useData";
+import genres from "../Data/genres";
+
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGenre = () => useData<Genre>("/genres");
+const useGenre = () => {return {data:genres, errors:null, isLoading:null}};
 
 export default useGenre;
